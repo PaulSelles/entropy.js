f17e072705f548341622c565fb3f8522b5c0d952
PaulSelles <PaulS@iqmetrix.com>
2013-04-06 17:16:48 -0600

    Initial check in

---
+++ test/spec/entropy.spec.js
@@ -0,0 +1,10 @@
+describe("Entropy", function () {
+
+    beforeEach(function () {
+        entropy = new Entropy();
+    });
+
+    it("should be created", function () {
+        expect(entropy).toEqual({});
+    });
+});
