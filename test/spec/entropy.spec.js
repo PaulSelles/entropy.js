db713bb8c2d23c97c786c53e2d24176dac5274f3
PaulSelles <PaulS@iqmetrix.com>
2013-04-07 08:59:11 -0600

    Added a few basic function and corresponding tests.

--- test/spec/entropy.spec.js
+++ test/spec/entropy.spec.js
@@ -1,10 +1,44 @@
+var testBitLength = 128;
+var defaultBitLength = 8;
+
 describe("Entropy", function () {
 
-    beforeEach(function () {
-        entropy = new Entropy();
+    describe("Default constructor", function () {
+
+        beforeEach(function () {
+            entropy = new Entropy();
+        });
+
+        it("should be created", function () {
+            expect(entropy).not.toEqual(null);
+        });
+
+        it("should have default bitLength", function () {
+            expect(entropy.getBitLength()).toEqual(8);
+        });
+
+        it("should have bitLength as a private variable", function () {
+            expect(entropy._bitLength).toEqual(undefined);
+        });
+
+        it("should not be ready after startup", function () {
+            expect(entropy.isReady()).toEqual(false);
+        });
+
+        it("should be able to change the preset bit length", function () {
+            entropy.setBitLength(testBitLength)
+            expect(entropy.getBitLength()).toEqual(testBitLength);
+        });
     });
 
-    it("should be created", function () {
-        expect(entropy).toEqual({});
+    describe("Defined biLegnth constructor", function () {
+
+        beforeEach(function () {
+            entropy = new Entropy(testBitLength);
+        });
+
+        it("should have defined bitLength", function () {
+            expect(entropy.getBitLength()).toEqual(testBitLength);
+        });
     });
 });
