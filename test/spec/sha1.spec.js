03fefc4945e88496983c96fcb1d0531a78388dd6
PaulSelles <PaulS@iqmetrix.com>
2013-04-11 21:55:26 -0600

    Added SHA-1 spec helper file. Tested all 32bit word blocks will all passes. Todo: test hashing main loop.

--- test/spec/sha1.spec.js
+++ test/spec/sha1.spec.js
@@ -28,39 +28,45 @@ describe("SHA1", function () {
 		block = sha1.encode("000000000000000000000000000000000000000000000000000000000000");
 		expect(sha1.preProcessedMessage.length * 8).toEqual(512*2);
 	});
-/*
-	it("should distribute intial 16 blocks properly", function() {
-		
-		var blocks = sha1.encode("");
 
-	 	expect(blocks.length).toEqual(1);
-		expect(blocks[0][0]).toEqual(0x80000000);
-		expect(blocks[0][1]).toEqual(0x00000000);
-		expect(blocks[0][15]).toEqual(0x00000000);
-		expect(blocks[0][80]).toEqual(undefined);
-		expect(blocks[1]).toEqual(undefined);
+	it("should distribute intial 16 blocks properly", function () {
 
-		blocks = sha1.encode("P@0P@0P@0P@0");
-	 	expect(blocks.length).toEqual(1);
-		expect(blocks[0][0]).toEqual(0x50403050);
-		expect(blocks[0][1]).toEqual(0x40305040);
-		expect(blocks[0][2]).toEqual(0x30504030);
-		expect(blocks[0][3]).toEqual(0x80000000);
-		expect(blocks[0][15]).toEqual(0x00000060);
-		expect(blocks[0][80]).toEqual(undefined);
-		expect(blocks[1]).toEqual(undefined);
+	    var blocks = sha1.encode("");
+	    testingBlock = testBlock("");
 
-		blocks = sha1.encode("00000000000000000000000000000000000000000000000000000000");
-		expect(blocks.length).toEqual(2);
-		expect(blocks[1][15]).toEqual(448);
-		expect(blocks[1][80]).toEqual(undefined);
-		expect(blocks[2]).toEqual(undefined);
+	    for (var i = 0; i < 80; i++) {
+	        expect(blocks[0][i] + "," + i).toEqual(testingBlock[i] + "," + i);
+	    }
+	    expect(blocks[0][80]).toEqual(undefined);
+	    expect(blocks[1]).toEqual(undefined);
+
+
+	    blocks = sha1.encode("P@0P@0P@0P@0");
+	    testingBlock = testBlock("P@0P@0P@0P@0");
+	    expect(blocks.length).toEqual(1);
+	    for (var i = 0; i < 80; i++) {
+	        expect(blocks[0][i] + "," + i).toEqual(testingBlock[i] + "," + i);
+	    }
+	    expect(blocks[0][80]).toEqual(undefined);
+	    expect(blocks[1]).toEqual(undefined);
+
+	    blocks = sha1.encode("00000000000000000000000000000000000000000000000000000000");
+	    testingBlock = testBlock("00000000000000000000000000000000000000000000000000000000");
+	    for (var i = 0; i < 2; i++) {
+	        for (var j = 0; j < 80; j++) {
+	            expect(blocks[i][j] + "," + i + "," + j).toEqual(testingBlock[i][j] + "," + i + "," + j);
+	        }
+	    }
+	    expect(blocks.length).toEqual(2);
+	    expect(blocks[1][15]).toEqual(448);
+	    expect(blocks[1][80]).toEqual(undefined);
+	    expect(blocks[2]).toEqual(undefined);
 	});
-*/
-	it("should hash properly", function () {
-		expect(sha1.encode("").length).toEqual(160/4);
-		expect(sha1.encode("")).toEqual("da39a3ee5e6b4b0d3255bfef95601890afd80709");
-		expect(sha1.encode("The quick brown fox jumps over the lazy dog")).toEqual("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12");
-		expect(sha1.encode("The quick brown fox jumps over the lazy cog")).toEqual("de9f2c7f d25e1b3a fad3e85a 0bd17d9b 100db4b3");
-	});
+
+	//it("should hash properly", function () {
+	//	expect(sha1.encode("").length).toEqual(160/4);
+	//	expect(sha1.encode("")).toEqual("da39a3ee5e6b4b0d3255bfef95601890afd80709");
+	//	expect(sha1.encode("The quick brown fox jumps over the lazy dog")).toEqual("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12");
+	//	expect(sha1.encode("The quick brown fox jumps over the lazy cog")).toEqual("de9f2c7f d25e1b3a fad3e85a 0bd17d9b 100db4b3");
+	//});
 });
